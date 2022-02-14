31a3490226f8ecd1a3312cefc65fbbfb05862f08
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-14 19:37:37 +0000

    Add function to initialize data picker and select field.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,7 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Sidenav.init(sidenav);
   });
 
+  // modal initialization
   document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+  });
+
+  // date picker initialization
+  document.addEventListener('DOMContentLoaded', function() {
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker);
+  });
+
+
+  // select field initialization
+  document.addEventListener('DOMContentLoaded', function() {
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
   });
