7d130cdf5f50b8220b4852d8963d121ce33ce659
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-15 15:29:10 +0000

    Add elements and icons through the index page.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -5,28 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // collapsible initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    let collapsible = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(collapsible);
-  });
+  let collapsible = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsible);
 
   // modal initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    let modal = document.querySelectorAll('.modal');
-    M.Modal.init(modal);
-  });
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+
 
   // date picker initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    let datePicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datePicker, {
-      format: "dd mmm yyyy",
-      i18n: {done: "Select"}
-    });
+  let datePicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datePicker, {
+    format: "dd mm, yyyy",
+    i18n: {done: "Select"}
   });
 
+
   // select field initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    let select = document.querySelectorAll('select');
-    M.FormSelect.init(select);
-  });
+  let select = document.querySelectorAll('select');
+  M.FormSelect.init(select);
